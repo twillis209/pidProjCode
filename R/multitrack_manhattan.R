0b2debbdfe9e1b57bbcc1375670692556c3ad605
Tom Willis <tw395@cam.ac.uk>
2021-04-09 22:20:37 +0100

    Added another parameter or two to multitrack_manhattan

--- R/multitrack_manhattan.R
+++ R/multitrack_manhattan.R
@@ -10,6 +10,7 @@
 #' @param axis_label_margin Size of margin between axis and its label
 #' @param main_title_cex Scaling factor for main title
 #' @param axis_label_cex Scaling factor for axis label
+#' @param axis_tick_cex Scaling factor for axis ticks
 #' @param chrom_names_cex Scaling factor for chromosome labels
 #' @param chromosomes List of chromosomes to plot. Expects strings of the form 'chrx' where 'x' is from the set {1, 2, ..., 22, X, Y} 
 #' @param chrom_tick_dist Distance in bp between ideogram ticks
@@ -20,7 +21,7 @@
 #' @importFrom karyoploteR plotKaryotype kpAddBaseNumbers kpAddChromosomeNames kpAddLabels kpAxis kpPlotManhattan getDefaultPlotParams autotrack
 #' @importFrom GenomicRanges mcols mcols<-
 #' @export
-multitrack_manhattan <- function(gRanges, axis_labels, main, ymax = 15, axis_label_margin = 0.03, main_title_cex = 2.7, axis_label_cex = 1.8, chrom_names_cex = 2, chromosomes = NULL, chrom_tick_dist=1e6, chrom_tick_cex = 1, plot_params = getDefaultPlotParams(plot.type = 4), track_margin = 0.06) {
+multitrack_manhattan <- function(gRanges, axis_labels, main, ymax = 15, axis_label_margin = 0.03, main_title_cex = 2.7, axis_label_cex = 1.8, axis_tick_cex = 1, chrom_names_cex = 2, chromosomes = NULL, chrom_tick_dist=1e6, chrom_tick_cex = 1, plot_params = getDefaultPlotParams(plot.type = 4), track_margin = 0.06) {
 
   for(i in seq_along(gRanges)) {
     if(!any(c('P', 'p') %in% names(mcols(gRanges[[i]])))) {
@@ -47,7 +48,7 @@ multitrack_manhattan <- function(gRanges, axis_labels, main, ymax = 15, axis_lab
                 cex = axis_label_cex, label.margin = axis_label_margin)
 
     kpAxis(kp, ymin = 0, ymax = ymax,
-           r0 = auto$r0, r1 = auto$r1)
+           r0 = auto$r0, r1 = auto$r1, cex = axis_tick_cex)
 
     kp <- kpPlotManhattan(kp, data = gRanges[[i]],
                         points.col = '2blues',
